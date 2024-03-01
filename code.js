a7d1ab97342add45f7ec57758333a9092ae13cbc
matheusmagri <161030849+matheusmagri@users.noreply.github.com>
2024-03-01 10:30:39 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+      const texto = document.body.querySelector("#texto");
+      texto.addEventListener("click", () => {
+       if (texto.textContent==="alô representante")
+           texto.textContent="E as camisas? nada ainda";
+        else
+            texto.textContent="alô representante";
+    
+    });
 
+});
